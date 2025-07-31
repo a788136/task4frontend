50902f1fa0124eb0dd8a5029800f2090328bc2e7
Artem Mamian <a788136@gmail.com>
2025-07-31 16:49:52 +0400

    back link

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:4000/api', // если backend локально
+  baseURL: 'https://task4backend-4a8r.onrender.com/api', // если backend локально
 });
 
 export default api;
